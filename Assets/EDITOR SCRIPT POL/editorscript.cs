63524e5d98af806a729c9025fcde39848c504f75
April <pol.lopez@estudiant.enti.cat>
2026-01-26 12:38:41 +0100

    MOVIMIENTO PERSONAJE + DETECCION ENEMIGOS
    Ninguna funciona aun porque falta pulir cosas, en el camino de vuelta a casa ire arreglandolo.

---
+++ Assets/EDITOR SCRIPT POL/editorscript.cs
@@ -0,0 +1,26 @@
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+[CustomEditor(typeof(EnemyScript))]
+public class editorscript : Editor
+{
+    void OnSceneGUI()
+    {
+        EnemyScript fow = (EnemyScript)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fow.transform.position, Vector3.forward, Vector3.right, 360, fow.viewRadius);
+        Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
+        Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+
+        Handles.color = Color.red;
+        foreach (Transform visibleTarget in fow.visibleTargets)
+        {
+            Handles.DrawLine(fow.transform.position, visibleTarget.position);
+        }
+    }
+}
